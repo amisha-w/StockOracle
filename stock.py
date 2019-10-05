55d342dc0676f42c245f2f94f26133b895110256
rahulmistry33 <rahul.mistry@somaiya.edu>
2019-10-05 15:15:55 +0530

    for loop indent repaired

--- stock.py
+++ stock.py
@@ -14,7 +14,7 @@ from iexfinance.stocks import get_historical_data
 
 def getStocks(n):
     #Navigating to the Yahoo stock screener    
-    chrome_driver_path = './chromedriver.exe'
+    chrome_driver_path = './chromedriver'
     driver = webdriver.Chrome(chrome_driver_path)
     url = 'https://finance.yahoo.com/screener/predefined/aggressive_small_caps?offset=0&count=202'
     driver.get(url)
@@ -22,8 +22,8 @@ def getStocks(n):
     stock_list = []
     n += 1
     for i in range(1, n):
-        ticker = driver.find_element_by_xpath('//*[@id="scr-res-table"]/div[1]/table/tbody/tr[' + str(i) + ']/td[1]/a')
-        stock_list.append(ticker.text)
+        ticker = driver.find_element_by_xpath('//*[@id="scr-res-table"]/div[1]/table/tbody/tr[  {}  ]/td[1]/a'.format(i))
+    stock_list.append(ticker.text)
 
     #Using the stock list to predict the future price of the stock a specificed amount of days
     for i in stock_list:
@@ -37,8 +37,11 @@ def predictData(stock,days):
     start = datetime(2017, 1, 1)
     end = datetime.now()
     #Outputting the Historical data into a .csv for later use
-    df = get_historical_data(stock, start=start, end=end,     output_format='pandas')
+    df = get_historical_data(stock, start=start, end=end,output_format='pandas')
+    print(df)
+    
     csv_name = ('Exports/' + stock + '_Export.csv')
+    
     df.to_csv(csv_name)
     df['prediction'] = df['close'].shift(-1)
     df.dropna(inplace=True)
